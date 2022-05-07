c09b864ffe111031085525649ac0d7e46592aea5
trinscloud <trinhamm@gmail.com>
2022-05-06 17:13:56 -0700

    structured-english

---
+++ test_2.py
@@ -0,0 +1,15 @@
+"""
+Structured English:
+
+This program will output 3 values; number of grades, average grade, and percentage of grades that are above average.
+
+In order to determine the number of grades in the class, the program will count the total number of items in the list.
+
+To calculate the average grade, the program will add the total scores and divide by the number of grades in the class.
+
+As for calculating the percentage of the above average grade, the program must depict whether the current grade being checked surpasses the average grade. The counter variable will allow us to keep track of the amount of grades that are above average and then the program will divide that number by the total number of grades in the class. 
+
+After calculating each value, the program will print out the values of Number of grades: , Average grade: , and Percentage of grades above average: 
+
+"""
+
