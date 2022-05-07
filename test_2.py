1af3032d926e5374fc950d7c2b7a4ce3574c6e9b
trinscloud <trinhamm@gmail.com>
2022-05-06 17:33:05 -0700

    pseudo-code

--- test_2.py
+++ test_2.py
@@ -13,3 +13,32 @@ After calculating each value, the program will print out the values of Number of
 
 """
 
+"""
+Pseudo-code:
+
+infile = open 
+#assign items in text to variable called grades
+grades = [line.rstrip()]
+
+#convert items in list to integer type 
+for items in Final.txt
+change grades at variable i to int
+
+grade_average = sum of total grade / # of grades in list
+initialize grade_average by dividing sum of the grade  and # of grades in list
+counter = 0   # placeholder for the numbers of grades that are above the average grade
+
+
+#Find the amount of grades that are above average
+for every grade in grades
+    if grade is greater than grade_average
+    increment counter by 1
+
+set grades_above_avg to: counter divided by total # of grades in the list, multiply by 100 to get percentage value
+
+print total amount of grades
+print average grade
+print percentage of grades above average
+
+
+"""
